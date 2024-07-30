a97320901654099d68adfa1ce6a3425610d25a3d
Laptop <guillermorosas00@gmail.com>
2024-07-29 22:54:53 -0600

    segundo commit se agregan ciertos archivos

--- src/main/java/com/cursojava/curso/CursoApplication.java
+++ src/main/java/com/cursojava/curso/CursoApplication.java
@@ -3,6 +3,8 @@ package com.cursojava.curso;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// estas anoccioncon @ al incicio , indican una funcuionalidad a lo que viene como clases o funcinoes
+// osea que la calse pincipal usara spring
 @SpringBootApplication
 public class CursoApplication {
 
