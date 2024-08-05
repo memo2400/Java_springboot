12bd355d7a3d926a741c12e0659d16ab400034b7
Laptop <memorosas.00@gmail.com>
2024-08-04 22:33:34 -0600

    se agregan dependencias para getter y setters

--- src/main/java/com/cursojava/curso/dao/UsuarioDaoImple.java
+++ src/main/java/com/cursojava/curso/dao/UsuarioDaoImple.java
@@ -6,9 +6,13 @@ import org.springframework.stereotype.Repository;
 
 import com.cursojava.curso.models.Usuario;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 
-/* transaccional da la funcionalidad de armar consultas AQL a la BD */
+/* transaccional da la funcionalidad de armar consultas SQL a la BD  como transaccion
+ * Repository = accede a repo de BD
+*/
 
 @Repository
 @Transactional
@@ -23,11 +27,29 @@ import jakarta.transaction.Transactional;
 
 public class UsuarioDaoImple implements UsuarioDao {
 
+    // contexto en la BD a usar
+    @PersistenceContext 
+    
+    // realiza la conexion a BD
+    EntityManager EntityManager;
+
     // el override es como un reemplazo
     @Override
     public List<Usuario> obtenerUsuario() {
+
+        /*
+         * hacemos consulta con hibernate
+         * sobre la clase
+         */
+
+        String consulta = "FROM usuario";
+
+        //creo consulta.guardo en lista
+
+        return EntityManager.createQuery(consulta).getResultList();
+ 
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'obtenerUsuario'");
+        // throw new UnsupportedOperationException("Unimplemented method 'obtenerUsuario'");
     }
 
 }
--- src/main/java/com/cursojava/curso/models/Usuario.java
+++ src/main/java/com/cursojava/curso/models/Usuario.java
@@ -1,55 +1,78 @@
 package com.cursojava.curso.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+// indicamos la tabla donse se guardara
+@Table(name = "usuarios")
+
+// hace referencia a una entidad de BD
+@Entity 
 public class Usuario {
 
+    /*
+     * decoradores de getter setter de lombok
+     * hacen codigo en menos lineas
+     */
+    @Getter @Setter
     private String nombre;
+    @Getter @Setter
     private String apellido;
-    private String email;
-    private String telefono;
-    private String password;
 
+    @Getter @Setter
+    private String email;
+    @Getter @Setter
+    private String telefono;
+
+    @Getter @Setter
+    private String password;
+    @Getter @Setter
     private Long id;
 
     /* usaremos la convencion de getters y setters oara esta app
      * hay otros como builders que es otra convencion 
+     * 
+     * ya no usaremos getters y setters 
      */
 
-     public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public String getApellido() {
-        return apellido;
-    }
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getTelefono() {
-        return telefono;
-    }
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
+    //  public String getNombre() {
+    //     return nombre;
+    // }
+    // public void setNombre(String nombre) {
+    //     this.nombre = nombre;
+    // }
+    // public String getApellido() {
+    //     return apellido;
+    // }
+    // public void setApellido(String apellido) {
+    //     this.apellido = apellido;
+    // }
+    // public String getEmail() {
+    //     return email;
+    // }
+    // public void setEmail(String email) {
+    //     this.email = email;
+    // }
+    // public String getTelefono() {
+    //     return telefono;
+    // }
+    // public void setTelefono(String telefono) {
+    //     this.telefono = telefono;
+    // }
+    // public String getPassword() {
+    //     return password;
+    // }
+    // public void setPassword(String password) {
+    //     this.password = password;
+    // }
+    // public Long getId() {
+    //     return id;
+    // }
+    // public void setId(Long id) {
+    //     this.id = id;
+    // }
 
 
     
