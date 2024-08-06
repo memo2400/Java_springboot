2e97a5b5f08802d1f423ad82b7f313ee98796b9d
Laptop <memorosas.00@gmail.com>
2024-08-05 22:30:14 -0600

    se mueve usuarios controller, se agrega dato a app properties

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -4,16 +4,27 @@ import java.nio.file.attribute.UserPrincipalLookupService;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cursojava.curso.dao.UsuarioDao;
 import com.cursojava.curso.models.Usuario;
 
 /* con agregar el deecorador se inporta lo demas de manera normal */
 @RestController
 public class UsuarioController {
 
+    /*
+     * hace que la clase usuarioDaoImplement 
+     * cree un objeto y la guarde en usuarioDao
+     * si usamos mucho el @autowired, el objeto
+     * estaria compartido, en memoria
+     * Esta es la inyeccion de dependencias
+     */
+    @Autowired
+    private UsuarioDao usuarioDao;
 
     /* le agregue el termino list y despues importe la lista 
      * para arreglar el error
@@ -50,45 +61,49 @@ public class UsuarioController {
 
     @RequestMapping (value = "usuarios")
     public List <Usuario> obtenerUsuarios () {
+
+        // Con esta linea hacemos la consulta a BD
+        return usuarioDao.obtenerUsuario();
         
-        // se crea un alista vacia de usuarios
+        // Toda esa parte se utilizo para hacer pruebas previo a la BD
+        // // se crea un alista vacia de usuarios
 
-        List <Usuario> usuarios  = new ArrayList<>();
+        // List <Usuario> usuarios  = new ArrayList<>();
         
-        Usuario usuario = new Usuario();
+        // Usuario usuario = new Usuario();
 
-        usuario.setNombre("Michael");
-        usuario.setApellido("Hansen");
-        usuario.setEmail("ejemplo@hotmail.com");
-        usuario.setTelefono("2711234321");
-        usuario.setId(117L);
+        // usuario.setNombre("Michael");
+        // usuario.setApellido("Hansen");
+        // usuario.setEmail("ejemplo@hotmail.com");
+        // usuario.setTelefono("2711234321");
+        // usuario.setId(117L);
 
-        // utilizo la L para evitar el error
+        // // utilizo la L para evitar el error
 
 
-        Usuario usuario2 = new Usuario();
+        // Usuario usuario2 = new Usuario();
 
-        usuario2.setNombre("Alan");
-        usuario2.setApellido("Poe");
-        usuario2.setEmail("restos@yahoo.com.com");
-        usuario2.setTelefono("5512344321");
-        usuario2.setId(114L);
+        // usuario2.setNombre("Alan");
+        // usuario2.setApellido("Poe");
+        // usuario2.setEmail("restos@yahoo.com.com");
+        // usuario2.setTelefono("5512344321");
+        // usuario2.setId(114L);
 
-        Usuario usuario3 = new Usuario();
+        // Usuario usuario3 = new Usuario();
 
-        usuario3.setNombre("Andi");
-        usuario3.setApellido("Deris");
-        usuario3.setEmail("holacasa@gmail.com");
-        usuario3.setTelefono("2248344321");
-        usuario3.setId(25L);
+        // usuario3.setNombre("Andi");
+        // usuario3.setApellido("Deris");
+        // usuario3.setEmail("holacasa@gmail.com");
+        // usuario3.setTelefono("2248344321");
+        // usuario3.setId(25L);
 
-        // se agregan los usuarios
+        // // se agregan los usuarios
 
-        usuarios.add(usuario);
-        usuarios.add(usuario2);
-        usuarios.add(usuario3);
+        // usuarios.add(usuario);
+        // usuarios.add(usuario2);
+        // usuarios.add(usuario3);
 
-        return usuarios;
+        // return usuarios;
         
     }
 
--- src/main/java/com/cursojava/curso/models/Usuario.java
+++ src/main/java/com/cursojava/curso/models/Usuario.java
@@ -15,6 +15,7 @@ import lombok.ToString;
 // hace referencia a una entidad de BD
 @Entity 
 
+// genera el to string de forma automatica para esta clase
 @EqualsAndHashCode @ToString
 public class Usuario {
 
